bd0b3e683acc429ecca40a27e5fb63c1d88214c8
YasminAlves16 <yasmin.alves16@outlook.com.br>
2022-04-08 04:08:14 -0300

    Style - mobile first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import './App.css';
-import { ProductsList } from './components/ProductsList'
-import {useState, useEffect} from 'react'
+import "./App.css";
+import ProductsList from "./components/ProductsList";
+import Cart from "./components/Cart";
+import { useState, useEffect } from "react";
+import fotoLogo from "./components/imgs/logo.svg";
 
 function App() {
+  const [products, setProducts] = useState([]);
 
-  const [products, setProducts] = useState('https://hamburgueria-kenzie-json-serve.herokuapp.com/products');
+  const [currentSale, setCurrentSale] = useState([]);
+
+  const [userInput, setUserInput] = useState("");
+
+  const [filteredProducts, setFilteredProducts] = useState(products);
 
   useEffect(() => {
-
     const fetchResponse = async () => {
-      const response = await fetch(products);
+      const response = await fetch(
+        "https://hamburgueria-kenzie-json-serve.herokuapp.com/products"
+      );
 
       const jsonResponse = await response.json();
 
@@ -17,26 +25,77 @@ function App() {
     };
 
     fetchResponse();
-  }, [products]);
+  }, []);
 
+  const showProducts = (produtoFiltrado) => {
+    const filtraPesquisa = products.filter((item) => {
+      return (
+        item.name.toUpperCase().includes(produtoFiltrado.toUpperCase()) ||
+        item.category.toUpperCase().includes(produtoFiltrado.toUpperCase())
+      );
+    });
+    setFilteredProducts(filtraPesquisa);
+  };
 
-  const showProducts = () => {
-    
-  }
+  const handleClick = (id) => {
+    const findProduct = products.find((product) => product.id === id);
+    const checkFindProduct = currentSale.find(
+      (cartProduct) => cartProduct.id === id
+    );
 
-  const handleClick = (productId) => {
-    
-  }
+    !checkFindProduct && setCurrentSale([...currentSale, findProduct]);
+  };
+
+  const handleClickRemove = (id) => {
+    const newCurrentSale = currentSale.filter((item) => item.id !== id);
+
+    setCurrentSale(newCurrentSale);
+  };
+
+  const removeAllItems = () => {
+    setCurrentSale([]);
+  };
 
   return (
-    
     <>
-    <header></header>
-    <main>
-      <ProductsList products={products}/>
-    </main>
-    </>
+      <header>
+        <img src={fotoLogo} alt="Logo Burguer Kenzie" />
+        <form>
+          <input type="text" placeholder="Digitar Pesquisa" />
+          <button> Pesquisar </button>
+        </form>
+      </header>
 
+      <main>
+        <section className="vitrine">
+          <ProductsList
+            products={products}
+            handleClick={handleClick}
+            children={"Adicionar"}
+          />
+        </section>
+
+        <section className="carrinho">
+          {currentSale.length >= 1 ? <div className='divProduct'> 
+            <h2>Carrinho de Compras</h2>
+          <ProductsList
+            products={currentSale}
+            handleClick={handleClickRemove}
+            children={"excluir"}
+          />
+          {currentSale.length > 0 && <Cart currentSale={currentSale} />}
+          <button className="deleteAll" onClick={removeAllItems}>Remover Todos </button></div>  :
+           <div className="divCarrinho">
+             <h2>Carrinho de Compras</h2>
+             <p>Sua Sacola Esta Vazia</p>
+             <span>Adicione Itens</span>
+           </div>
+
+           }
+         
+        </section>
+      </main>
+    </>
   );
 }
 
